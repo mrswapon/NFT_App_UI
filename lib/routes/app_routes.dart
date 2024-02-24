86c8416fff47d2e7e84a56e6ffffa1d53fe7da0d
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-02-24 10:08:57 +0600

    create files

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static const String homeScreen = "/home_screen";
+
+
+  static List<GetPage> get routes => [
+    //GetPage(name: homeScreen, page: () => HomeScreen()),
+
+  ];
+}
