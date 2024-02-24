c4e4003cd471213f7706aaca32a307092f0699f4
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-02-24 14:21:39 +0600

    WelcomeScreen Completed

--- lib/views/screens/welcome_screen.dart
+++ lib/views/screens/welcome_screen.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/animation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -40,60 +38,64 @@ class WelcomeScreen extends StatelessWidget {
                 width: double.infinity,
                 height: 191.h,
                 decoration: BoxDecoration(
-                  border: Border.all(color: const Color(0xFF7e6487), width: 1.w),
+                  border:
+                      Border.all(color: const Color(0xFF7e6487), width: 1.w),
                   color: Colors.transparent,
                   borderRadius: BorderRadius.circular(27.r),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(27.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CustomText(
-                        text: AppConstants.explore,
-                        fontWeight: FontWeight.w700,
-                        fontsize: 19.h,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 5.h),
-                      CustomText(
-                        text: AppConstants.youCan,
-                        fontWeight: FontWeight.w400,
-                        fontsize: 11.h,
-                        maxline: 2,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 20.h),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CustomText(
+                      text: AppConstants.explore,
+                      fontWeight: FontWeight.w700,
+                      fontsize: 19.h,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 5.h),
+                    CustomText(
+                      text: AppConstants.youCan,
+                      fontWeight: FontWeight.w400,
+                      fontsize: 11.h,
+                      color: Colors.white,
+                    ),
+                    CustomText(
+                      text: AppConstants.artists,
+                      fontWeight: FontWeight.w400,
+                      fontsize: 11.h,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 20.h),
 
-                      //=================================> Get Started Button <========================
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          width: 198.w,
-                          height: 45.h,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: const Color(0xFF6b6ccd), width: 1.w),
-                            gradient: const LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.topRight,
-                              colors: [Color(0xFF664a79), Color(0xFF5251a7)],
-                            ),
-                            color: const Color(0xFF654975),
-                            borderRadius: BorderRadius.circular(33.r),
+                    //=================================> Get Started Button <========================
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 198.w,
+                        height: 45.h,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color(0xFF6b6ccd), width: 1.w),
+                          gradient: const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.topRight,
+                            colors: [Color(0xFF664a79), Color(0xFF5251a7)],
                           ),
-                          child: Center(
-                            child: CustomText(
-                              text: AppConstants.getStarted,
-                              fontsize: 15.h,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
+                          color: const Color(0xFF654975),
+                          borderRadius: BorderRadius.circular(33.r),
+                        ),
+                        child: Center(
+                          child: CustomText(
+                            text: AppConstants.getStarted,
+                            fontsize: 15.h,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
                           ),
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
